571a5b718acea1e31309cc59a81bbbc9e1209276
Wesley.S.Godoy <wpggodoi@gmail.com>
2025-07-13 14:42:39 -0300

    currently-working-in-player-animation
    yes.

---
+++ Dino run project/Assets/script/backgroundMovement.cs
@@ -0,0 +1,29 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class backgroundMovement : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer _meshRenderer;
+
+    [SerializeField] private float animateSpeed = 1f;
+
+
+
+
+    void Awake()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void  Update()
+    {
+        _meshRenderer.material.mainTextureOffset += new Vector2(animateSpeed * Time.deltaTime, 0);
+    }
+}
+
+
+    
+    
+    
+
+
